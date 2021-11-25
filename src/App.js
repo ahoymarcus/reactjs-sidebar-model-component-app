0cb930d556000fb34f0c1d4ba79bf8acd9f618c8
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 18:17:52 -0300

    Montando a estrutura Jsx basica para App.js e para os componentes Home e Modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 // https://www.youtube.com/watch?v=ly3m6mv5qvg
 // 5 hr 17' 30 ''
 import React from 'react'
+
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
+
+
+
 function App() {
+
+
   return (
     <>
-      <h2>modal-sidebar project setup</h2>
+      <Home />
+      <Modal />
+      <Sidebar />
     </>
   )
 }
 
+
+
 export default App
+
+
