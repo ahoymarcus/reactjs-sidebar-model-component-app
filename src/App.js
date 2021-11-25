81d06297b45f3cda802a29e7cf0284f129ce4349
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 18:10:33 -0300

    Iniciar repositorio local

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 5 hr 17' 30 ''
+import React from 'react'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  return (
+    <>
+      <h2>modal-sidebar project setup</h2>
+    </>
+  )
+}
+
+export default App
