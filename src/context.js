71f5d5c74a98ea92eda90808fd3839789d254375
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 18:42:55 -0300

    Definido o componente Context.js e definindo o Contexto e o Provider.

--- src/context.js
+++ src/context.js
@@ -1,1 +1,16 @@
 import React, { useState, useContext } from 'react'
+
+
+const AppContext = React.createContext();
+
+
+/* 
+    Atenção: usar o objeto Children é essencial para se alcançar os demais componentes.
+*/
+const AppProvider = ({ children } ) => {
+    return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+};
+
+
+
+export { AppContext, AppProvider }
