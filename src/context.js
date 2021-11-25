bf07564148812b9af085a9eaae2519a83b0fceed
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 18:49:25 -0300

    Fazer a importacao do objeto AppProvider em index.js e fazer o wrapper do componente App.js entre as tags do provider.

--- src/context.js
+++ src/context.js
@@ -7,7 +7,7 @@ const AppContext = React.createContext();
 /* 
     Atenção: usar o objeto Children é essencial para se alcançar os demais componentes.
 */
-const AppProvider = ({ children } ) => {
+const AppProvider = ({ children }) => {
     return <AppContext.Provider value="hello">{children}</AppContext.Provider>
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { AppProvider } from './context';
 import App from './App'
+
+
+
+/* 
+  Lembrando que AppProvider teve de receber o obj Children como param e retorná-lo também, a partir desse momento todo todos os componentes de App.js podem acessar os valores controlados por AppProvider.....
+*/
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+
